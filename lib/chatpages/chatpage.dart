cd69e9bc361dbf84b6bf8078d735c971558b1867
sangeethck <sangeethck7@gmail.com>
2022-06-02 10:58:12 +0530

    stpe3

--- lib/chatpages/chatpage.dart
+++ lib/chatpages/chatpage.dart
@@ -48,18 +48,18 @@ List<Welcome> chatlist=[
    name: "Mr.X", message: "in this video ,i'm demonstrating protopie for mobile app animation and intraction..", time: "10:30", isgroup: "true"),
 
    Welcome(avatar: "https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_1280.png",
-   name: "Mr.Y", message: "Hi  happy to see after long time", time: "10:50", isgroup: "true"),
+   name: "Mr.D", message: "Hi  happy to see after long time", time: "10:50", isgroup: "true"),
 
     Welcome(avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnPGqX4s6HDBoVTLwIhy3fFmdxvMiDIfUtdA&usqp=CAU",
-   name: "Mr.Y", message: "Hi its me Cristna", time: "10:20", isgroup: "true"),
+   name: "Mr.M", message: "Hi its me Cristna", time: "10:20", isgroup: "true"),
 
     Welcome(avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdzwVTM2Qz3A7SqUsdzLsUBfjtz4y5xgAyZw&usqp=CAU",
-   name: "Mr.Y", message: "Hi welcome", time: "8:30", isgroup: "true"),
+   name: "Mr.E", message: "Hi welcome", time: "8:30", isgroup: "true"),
 
     Welcome(avatar: "https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_1280.png",
-   name: "Mr.Y", message: "Hi CV for Telegram.pdf", time: "6:10", isgroup: "true"),
+   name: "Mr.X", message: "Hi CV for Telegram.pdf", time: "6:10", isgroup: "true"),
     Welcome(avatar: "https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_1280.png",
-   name: "Mr.Y", message: "Hi  its photo zip file", time: "9:30", isgroup: "true"),
+   name: "Mr.M", message: "Hi  its photo zip file", time: "9:30", isgroup: "true"),
 ];
 
 List<MessageModel> chatsMe = [
